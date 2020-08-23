457f52642cc506b95cdf4e6f3da806086e4f2e91
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-23 20:19:15 +0700

    add product & order routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard');
 });
+
+Route::get('/product', function() {
+    return view('product');
+});
+
+Route::get('/order', function() {
+    return view('order');
+});
