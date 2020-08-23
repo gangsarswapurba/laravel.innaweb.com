95c44008911d2c5ab2a52ae71cf18faf28b718cf
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-23 23:19:27 +0700

    adding some meta into head html

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,6 +2,11 @@
 <html>
 <head lang="{{ config('app.locale') }}">
     <title>{{ config('app.name') }}</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Jamaluddin Rumi">
     @include('/components/favicon')
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
