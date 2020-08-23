a8cdcaf0b88df603ddc579ef9a07894f544a97b4
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-23 19:40:15 +0700

    done setup header main container & footer

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -27,6 +27,19 @@
         </nav>
     </header>
 
+    <main class="container container-person mt-5 p-5 shadow">
+        
+    </main>
+
+    <footer class="page-footer font-small">
+        <div class="footer-copyright text-center py-3">
+            <p class="font-weight-light mb-0">
+                <span>&copy; {{ date('Y') }} Copyright:</span>
+                <a href="https://algostudio.net/"> algostudio</a>
+            </p>
+        </div>
+    </footer>
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
