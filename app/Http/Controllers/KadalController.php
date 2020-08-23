60cf1a96232c47f46a2f327b517372e81be4caf2
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-23 23:20:50 +0700

    check db conn by print order table

--- app/Http/Controllers/KadalController.php
+++ app/Http/Controllers/KadalController.php
@@ -7,11 +7,13 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+use Illuminate\Support\Facades\DB;
+
 class KadalController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     function render() {
-        return 'halo ini kadal';
+        return DB::table('order')->get();
     }
 }
