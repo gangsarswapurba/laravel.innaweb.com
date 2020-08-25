3b5c1b747417e4247adcf187171c0cbf59636d03
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-25 15:34:50 +0700

    build prod

--- resources/views/order/create.blade.php
+++ resources/views/order/create.blade.php
@@ -10,7 +10,7 @@
         <h1>Tambah Transaksi Penjualan</h1>
     @endif
     @if($products) 
-    <form id="form_make_order" method="post" action="{{ $action_form }}">
+    <form id="form_make_order" method="post" enctype="multipart/form-data" action="{{ $action_form }}">
         {{ @csrf_field() }}
         @foreach ($products as $product)
             <div class="container">
