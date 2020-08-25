8005645f87b24b0df8963493d30aba9297cf614b
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-25 09:40:57 +0700

    build dev

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,209 @@
-!function(e){var t={};function r(o){if(t[o])return t[o].exports;var n=t[o]={i:o,l:!1,exports:{}};return e[o].call(n.exports,n,n.exports,r),n.l=!0,n.exports}r.m=e,r.c=t,r.d=function(e,t,o){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:o})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var o=Object.create(null);if(r.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var n in e)r.d(o,n,function(t){return e[t]}.bind(null,n));return o},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="/",r(r.s=0)}({0:function(e,t,r){r("bUC5"),r("hj34"),r("lzph"),e.exports=r("pyCd")},bUC5:function(e,t){},hj34:function(e,t){$(document).ready((function(){$("table").DataTable(),$("#descricao").summernote({toolbar:[["style",["bold","italic","underline","clear"]],["font",["strikethrough","superscript","subscript"]],["fontsize",["fontsize"]],["color",["color"]],["para",["ul","ol","paragraph"]],["height",["height"]]],disableDragAndDrop:!0}),$(".delete-product").click((function(){var e=$(this).data("id");$(".modal-footer #confirmDeleteProduct").attr("href",e)})),$(".delete-order").click((function(){var e=$(this).data("id");$(".modal-footer #confirmDeleteOrder").attr("href",e)}))}))},lzph:function(e,t){for(var r,o=[],n=(new Date).getDate(),a=new Date((new Date).getYear(),(new Date).getMonth(),0).getDate(),l=[],i=0;i<a;i++)o[i]=i<n?(r=10,Math.floor(Math.random()*Math.floor(r))):0;for(var u=1;u<=a;u++)l.push(u);var c=document.getElementById("myChart");if(null!==c){var d=c.getContext("2d");new Chart(d,{type:"bar",data:{labels:l,datasets:[{label:"Penjualan tanggal ke #",data:o,borderWidth:1,backgroundColor:"#71c7ec"}]},options:{scales:{yAxes:[{ticks:{beginAtZero:!0}}]}}})}},pyCd:function(e,t){}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./resources/js/app.js":
+/*!*****************************!*\
+  !*** ./resources/js/app.js ***!
+  \*****************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+
+
+/***/ }),
+
+/***/ "./resources/js/chart.js":
+/*!*******************************!*\
+  !*** ./resources/js/chart.js ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+var amount_orders = [];
+var to_date = new Date().getDate();
+var days_in_month = new Date(new Date().getYear(), new Date().getMonth(), 0).getDate();
+var days_label = [];
+
+for (var i = 0; i < days_in_month; i++) {
+  if (i < to_date) {
+    amount_orders[i] = getRandomInt(10);
+  } else {
+    amount_orders[i] = 0;
+  }
+}
+
+for (var j = 1; j <= days_in_month; j++) {
+  days_label.push(j);
+}
+
+var myChartElement = document.getElementById("myChart");
+
+if (myChartElement !== null) {
+  var ctx = myChartElement.getContext("2d");
+  var myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: days_label,
+      datasets: [{
+        label: "Penjualan tanggal ke #",
+        data: amount_orders,
+        borderWidth: 1,
+        backgroundColor: "#71c7ec"
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+}
+
+/***/ }),
+
+/***/ "./resources/js/datatables.js":
+/*!************************************!*\
+  !*** ./resources/js/datatables.js ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+$(document).ready(function () {
+  $("table").DataTable();
+  $('#descricao').summernote({
+    toolbar: [// [groupName, [list of button]]
+    ['style', ['bold', 'italic', 'underline', 'clear']], ['font', ['strikethrough', 'superscript', 'subscript']], ['fontsize', ['fontsize']], ['color', ['color']], ['para', ['ul', 'ol', 'paragraph']], ['height', ['height']]],
+    disableDragAndDrop: true
+  });
+  $('.delete-product').click(function () {
+    var delete_url = $(this).data('id');
+    $(".modal-footer #confirmDeleteProduct").attr("href", delete_url);
+  });
+  $('.delete-order').click(function () {
+    var delete_url = $(this).data('id');
+    $(".modal-footer #confirmDeleteOrder").attr("href", delete_url);
+  });
+});
+
+/***/ }),
+
+/***/ "./resources/sass/app.scss":
+/*!*********************************!*\
+  !*** ./resources/sass/app.scss ***!
+  \*********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 0:
+/*!******************************************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/js/datatables.js ./resources/js/chart.js ./resources/sass/app.scss ***!
+  \******************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! /Volumes/HDD/gangsar/laravel/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /Volumes/HDD/gangsar/laravel/resources/js/datatables.js */"./resources/js/datatables.js");
+__webpack_require__(/*! /Volumes/HDD/gangsar/laravel/resources/js/chart.js */"./resources/js/chart.js");
+module.exports = __webpack_require__(/*! /Volumes/HDD/gangsar/laravel/resources/sass/app.scss */"./resources/sass/app.scss");
+
+
+/***/ })
+
+/******/ });
