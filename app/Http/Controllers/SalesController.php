40d105bb361b361943a2540b6e25a0f00ddc9cb0
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-26 12:37:56 +0700

    return the right one; the last month sales by day results

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -9,16 +10,29 @@ class SalesController extends Controller
 {
     
     function lastMonth() {
-        $sales = DB::table('product')
+        $products = DB::table('product')
         ->join('product_order', 'product_order.product_id', '=', 'product.id')
         ->join('order', 'product_order.order_id', '=', 'order.id')
-        ->select('order.data as tanggal', 'product.nome as nama', 'product.sku', 'product.preco as harga', 'product_order.product_qtd as quantity')
+        ->select('order.data as tanggal', 'product_order.order_id', 'product.nome as nama', 'product.sku', 'product.preco as harga', 'product_order.product_qtd as quantity')
         ->orderBy('order.id')
         ->where('order.status', '=', 1)
         ->whereBetween('data', [date('Y-m-01'), date('Y-m-t')])
         ->distinct()
         ->get();
 
-        return $sales;
+        $orders = [];
+        foreach ($products as $key => $product){
+            $date = new DateTime($product->tanggal);
+            $theDate = $date->format('d');
+
+            if (isset($orders[$theDate])) {
+                $orders[$theDate] += $product->quantity;
+            } else {
+                $orders[$theDate] = $product->quantity;
+            }
+
+        }
+
+        return $orders;
     }
 }
